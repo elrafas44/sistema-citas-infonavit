a7c58c43181591a5c3232d84d8bcfc0d63cd3fe6
elrafas44 <rafitroxi@gmail.com>
2026-05-18 19:31:42 -0600

    Implementación de Security Headers en Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,46 @@
 /** @type {import('next').NextConfig} */
+
+
+const securityHeaders = [
+  {
+    // Previene el Clickjacking (Evita que metan la página en un iframe malicioso)
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    // Fuerza a los navegadores a respetar los tipos de archivo (MIME types)
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    // Activa el filtro XSS integrado en navegadores antiguos/compatibles
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    // Controla cuánta información se envía cuando el usuario hace clic en un enlace hacia OTRA web
+    key: 'Referrer-Policy',
+    value: 'strict-origin-when-cross-origin',
+  },
+  {
+    // Le dice a los navegadores que SOLO se conecten a esta web usando HTTPS (vital para producción)
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains; preload',
+  }
+];
+
 const nextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  // Le decimos a Next.js que aplique estas cabeceras a TODAS las rutas (/(.*))
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ];
+  },
 };
+
 
 export default nextConfig;
